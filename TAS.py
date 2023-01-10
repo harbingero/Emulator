6cb4746107a5c720aad645cda406a5aa79f89359
harbingero <77740620+harbingero@users.noreply.github.com>
2023-01-10 13:10:10 -0600

    Add files via upload

--- TAS.py
+++ TAS.py
@@ -1,21 +1,30 @@
 import struct
 from pyboy import PyBoy
 from pyboy import WindowEvent
+appended = ""
 
 pyboy = PyBoy('Roms/Pokemon Red.gb')
 
 for i in range(2000):
     pyboy.tick()
+    check = appended
+    appended = ""
+    for j in range(10000):
+        appended += str(pyboy.get_memory_value(j)) + ": "
+    if check != appended:
+        print(check, "\n" + appended)
+    else:
+        print("Same")
 else:
+    # pyboy.send_input(WindowEvent.PRESS_BUTTON_START)
+    # pyboy.tick()
+    # pyboy.send_input(WindowEvent.RELEASE_BUTTON_START)
+    #
+    # pil_image = pyboy.screen_image()
+    # pil_image.save('screenshot.png')
     pyboy.stop(save=False)
 # while not pyboy.tick():
 #     pass
 # pyboy.stop(save=False)
 #
 #
-# pyboy.send_input(WindowEvent.PRESS_BUTTON_START)
-# pyboy.tick()
-# pyboy.send_input(WindowEvent.RELEASE_BUTTON_START)
-#
-# pil_image = pyboy.screen_image()
-# pil_image.save('screenshot.png')
