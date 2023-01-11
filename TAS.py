06630fff3944c28dc9ab03681ae4900fcf4e17a2
harbingero <77740620+harbingero@users.noreply.github.com>
2023-01-11 16:46:21 -0600

    Add files via upload

--- TAS.py
+++ TAS.py
@@ -1,27 +1,33 @@
 import struct
+import colorama
+from colorama import Fore
 from pyboy import PyBoy
 from pyboy import WindowEvent
 appended = ""
 
 pyboy = PyBoy('Roms/Pokemon Red.gb')
+increment = 0
+game_start = "Game_Start.png"
+new_game = "New_Game.png"
 
 for i in range(2000):
     pyboy.tick()
     check = appended
     appended = ""
-    for j in range(10000):
+    for j in range(16300, 16400):
         appended += str(pyboy.get_memory_value(j)) + ": "
     if check != appended:
-        print(check, "\n" + appended)
+        print(check, "\n" + Fore.GREEN + appended + Fore.RESET)
     else:
         print("Same")
+    # if i % 50 == 0:
+    #     increment += 1
+    #     pil_image = pyboy.screen_image()
+    #     pil_image.save('screenshot' + str(increment) + '.png')
 else:
     # pyboy.send_input(WindowEvent.PRESS_BUTTON_START)
     # pyboy.tick()
     # pyboy.send_input(WindowEvent.RELEASE_BUTTON_START)
-    #
-    # pil_image = pyboy.screen_image()
-    # pil_image.save('screenshot.png')
     pyboy.stop(save=False)
 # while not pyboy.tick():
 #     pass
